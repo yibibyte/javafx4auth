d6826fc2d04b55684607903c2ee380b10aa70846
ilmanuser <email@example.com>
2025-02-13 14:23:36 +0300

    add photo user

--- src/main/java/ru/japp/j4bot/UserController.java
+++ src/main/java/ru/japp/j4bot/UserController.java
@@ -2,10 +2,12 @@ package ru.japp.j4bot;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class UserController {
 
@@ -19,22 +21,51 @@ public class UserController {
     private Label ageUser;
 
     @FXML
+    private TextField ageUserField;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
     private Label emailUser;
+
+    @FXML
+    private TextField emailUserField;
 
     @FXML
     private Label firstNameUser;
 
     @FXML
+    private TextField firstNameUserField;
+
+    @FXML
     private Label lastNameUser;
 
     @FXML
-    private Label middleNameUser;
+    private TextField lastNameUserField;
+
+    @FXML
+    private ImageView photoUser;
 
     @FXML
     private Button saveFields;
+
+    // Метод для установки данных пользователя
+    public void setUserData(String name, String email, int age, String imageUrl) {
+        firstNameUserField.setText(name);
+        emailUserField.setText(email);
+        ageUserField.setText("Возраст: " + age);
+
+        // Загрузка изображения
+        Image image = new Image(imageUrl);
+        photoUser.setImage(image);
+    }
 
     @FXML
     void initialize() {
 
     }
+
 }
+
+
